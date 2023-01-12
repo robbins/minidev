9acf127f0bea4148c5ac5c5e060e3cbf65f6d969
Nathanael Robbins <nejrobbins@gmail.com>
2023-01-11 21:29:17 -0500

    Create flake.nix & use fzy from nixpkgs instead of a prebuilt binary so that it works on NixOS

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Flakeified minidev";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }: 
+  let
+    pkgs = import nixpkgs { system = "x86_64-linux"; };
+  in {
+    packages.x86_64-linux.minidev = pkgs.stdenvNoCC.mkDerivation {
+      pname = "minidev";
+      version = "1.2.0";
+      src = ./.;
+      buildInputs = [ pkgs.ruby ];
+      installPhase = ''
+        mkdir $out
+        rm vendor/fzy_*
+        ln -s ${pkgs.fzy}/bin/fzy vendor/fzy_${if pkgs.stdenv.isDarwin then "darwin" else "linux"}
+        cp -R $src/dev.sh $src/bin $src/lib vendor $out
+        chmod +x $out/bin/dev
+      '';
+    };
+    packages.x86_64-linux.default = self.packages.x86_64-linux.minidev;
+  };
+}
